9562ad594ab5ddbae3a113da2c10d1bb6f23ff6e
hira-21 <hirasaleemmubashar@gmail.com>
2025-11-11 22:20:23 +0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        chill: "#FDE68A",
+        reflective: "#FCA5A5",
+        hype: "#5EEAD4"
+      }
+    }
+  },
+  plugins: []
+}
